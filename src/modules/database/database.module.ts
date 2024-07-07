c994e7d62d70b4e2ec1dedd7d66975d4bc00d297
rv-cristoper <cristoper.runco@gmail.com>
2024-07-07 10:54:59 -0500

    fix plugin

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,14 +1,8 @@
-// import { Logger, Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import * as mongoosePaginate from 'mongoose-paginate-v2';
-// import { ConfigurationService } from 'src/config/configuration.service';
-// import { Connection } from 'mongoose';
 
-import { Logger, Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Connection } from 'mongoose';
+
 import 'mongoose-paginate-v2';
-import { ConfigurationService } from '../../config/configuration.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import configuration from '../../config/configuration';
 
 @Module({
@@ -22,25 +16,6 @@ import configuration from '../../config/configuration';
                 },
             }),
         }),
-        // MongooseModule.forRootAsync({
-        //     useFactory: async (configurationService: ConfigurationService) => ({
-        //         uri: configuration().mongoUri,
-        //         connectionFactory: (connection: Connection) => {
-        //             if (connection.readyState === 1) {
-        //                 Logger.log(`MongoDB Connected with: ${connection.host}`);
-        //             }
-        //             connection.on('disconnected', () => {
-        //                 Logger.warn('DB disconnected');
-        //             });
-        //             connection.on('error', (error) => {
-        //                 Logger.error(`DB connection failed! for error: ${error}`);
-        //             });
-        //             // connection.plugin(mongoosePaginate);
-        //             return connection;
-        //         },
-        //     }),
-        //     // inject: [ConfigurationService],
-        // }),
     ],
 })
 export class DatabaseModule { }
