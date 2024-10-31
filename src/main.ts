5b75e3844341634b088fd775d7b2fe5c9f04d76d
cristoper <cristoper.runco@banexcoin.com>
2024-10-31 18:04:28 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import configuration from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
 
   app.setGlobalPrefix('api/v1');
 
